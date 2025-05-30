fbdf9d62ddc1aa40a508cbb27147a5d6215a4064
Willian do Prado Vieira <willian.prado@ymail.com>
2025-05-30 16:18:31 -0300

    Migrate database from MySQL to PostgreSQL (Neon)

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'embrapa_secret'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/ambrapa_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_3w8CmUJnYbMo@ep-square-dream-actspwqu-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USER_CREATION_SECRET = 'chave_super_secreta'  # chave para autorizar criação de usuário
